91457512fb8a05ea527dd176cbf44a19ce8f82d2
Rea Sokoli <rea.sokoli@base-com.al>
2022-02-03 10:52:29 +0100

    adding files.-m

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Superman' },
+  { id: 2, name: 'Spiderman' },
+  { id: 3, name: 'Wonder Woman' },
+  { id: 4, name: 'Batman' },
+  { id: 5, name: 'Hulk' },
+  { id: 6, name: 'Captain America' },
+  { id: 7, name: 'Iron Man' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Aquaman' },
+  { id: 10, name: 'Thor' }
+];
